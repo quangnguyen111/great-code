0080e23b666d239c32ccc7a291747d726386e4de
Quang Nguyen <warofpigs@yahoo.com>
2016-10-12 23:42:48 +0700

    Add person code

---
+++ src/main/java/Person.java
@@ -0,0 +1,19 @@
+
+public class Person {
+	private String name;
+	private double height;
+	private double weight;
+	
+	public Person() {
+	}
+
+	public Person(String name, double height, double weight) {
+		this.name = name;
+		this.height = height;
+		this.weight = weight;
+	}
+	
+	public double getBMI() {
+		return this.weight / (this.height * this.height);
+	}
+}
---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,19 @@
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class PersonTest {
+
+	@Test
+	public void testGetBMI() {
+		Person nam = new Person("Nam", 1.62, 62);
+		Person trinh = new Person("Ngoc Trinh", 1.7, 49);
+		
+		Assert.assertEquals(nam.getBMI(), 23.6, 0.1);
+		Assert.assertEquals(trinh.getBMI(), 16.9, 0.1);
+		
+		
+	}
+
+}
