b908a14f3eaf5a3e4f488bd9959e158410055ce4
Hoang Minh Tuoi <Hoang Minh Tuoi>
2016-10-13 19:55:19 +0700

    modify PersonTest.java for testing

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -10,7 +10,7 @@ public class PersonTest {
 		Person nam = new Person("Nam", 1.62, 62);
 		Person trinh = new Person("Ngoc Trinh", 1.7, 49);
 
-		Assert.assertEquals(nam.getBMI(), 20, 0.1);
+		Assert.assertEquals(nam.getBMI(), 23.4, 0.1);
 		Assert.assertEquals(trinh.getBMI(), 16.9, 0.1);
 
 
