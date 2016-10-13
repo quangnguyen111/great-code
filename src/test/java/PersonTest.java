831b38ce1adbd992dd14aca83b7068591df0d390
Hoang Minh Tuoi <Hoang Minh Tuoi>
2016-10-13 17:14:28 +0700

    test autobuild

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -9,11 +9,11 @@ public class PersonTest {
 	public void testGetBMI() {
 		Person nam = new Person("Nam", 1.62, 62);
 		Person trinh = new Person("Ngoc Trinh", 1.7, 49);
-		
+
 		Assert.assertEquals(nam.getBMI(), 23.6, 0.1);
 		Assert.assertEquals(trinh.getBMI(), 16.9, 0.1);
-		
-		
+
+
 	}
 
 }
