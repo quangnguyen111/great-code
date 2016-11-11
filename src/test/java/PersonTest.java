8fde7239b6fee77b7a0afaa28294685503a02178
quang.d.nguyen <quang.d.nguyen@192.168.122.134>
2016-11-11 11:32:25 +0700

    New test ngon trinh

---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PersonTest {
+
+	@Test
+	public void test() {
+		Person ngocTRinh = new Person("Ngoc Trinh", 170, 49);
+	}
+
+}
