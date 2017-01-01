84dd3c5a8ab51945661861016ee66c1b69fda6e8
Quang Nguyen <warofpigs@yahoo.com>
2017-01-01 07:19:31 +0700

    Revert code.

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,13 +1,26 @@
 
 public class Person {
 	private String name;
-	private int height;
-	private int weight;
+	private double height;
+	private double weight;
 	
-	public Person(String name, int height, int weight) {
+	public Person() {
+	}
+
+	public Person(String name, double height, double weight) {
 		this.name = name;
 		this.height = height;
 		this.weight = weight;
 	}
+	
+	public double getBMI() {
+		return this.weight / (this.height * this.height);
+	}
 
+	public String getBodyStatus() {
+		if(this.getBMI() >= 22) {
+			return "Overweight";
+		}
+		return "Normal";
+	}
 }
