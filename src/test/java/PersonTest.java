c6f866e9d76662a7234d4001f66b54de34f4daea
Dell <Dell@DESKTOP-2PLKO9E>
2017-01-01 08:16:34 +0700

    Add missing file.

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -2,6 +2,29 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+class Person {
+	private String name;
+	private double height;
+	private double weight;
+	
+	public Person(String name, double height, double weight) {
+		this.name = name;
+		this.height = height;
+		this.weight = weight;
+	}
+	
+	public double getBMI() {
+		return this.weight / (this.height * this.height);
+	}
+
+	public String getBodyStatus() {
+		if(this.getBMI() >= 22) {
+			return "Overweight";
+		}
+		return "Normal";
+	}
+}
+
 
 public class PersonTest {
 
