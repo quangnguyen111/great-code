d02cc4d4b5b01074449a39468ea60dcfea097768
Dell <Dell@DESKTOP-2PLKO9E>
2017-01-12 11:42:38 +0700

    Add getBMI method

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -6,11 +6,22 @@ import org.junit.Test;
 	private double height;
 	private double weight;
 	
+	public Person() {
+	}
+	
+	public Person(double height, double weight) {
+		this.height = height;
+		this.weight = weight;
+	}
+	
 	public void setHeight(double height) {
 		this.height = height;
 	}
-	public void setWeight(double d) {		// TODO Auto-generated method stub
+	public void setWeight(double d) {	
 		this.weight = weight;
+	}
+	public double getBMI() {
+		return this.weight / (this.height * this.height);
 	}
 
 }
@@ -19,8 +30,10 @@ import org.junit.Test;
 public class PersonTest {
 	@Test
 	public void testGetBMI() {
-		Person sut = new Person();
-		sut.setHeight(180.3); 
-		sut.setWeight(72.0);
+		Person sut = new Person(1.8, 72.0);
+		Assert.assertEquals(22.22, sut.getBMI(), 0.01);
+		
+		Person quang = new Person(1.62, 62.0);
+		Assert.assertEquals(23.62, quang.getBMI(), 0.01);
 	}
 }
