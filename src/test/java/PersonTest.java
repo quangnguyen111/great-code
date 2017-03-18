d347bf1d92d4cd02c7a4b129c6d060926a4698a2
Dell <Dell@DESKTOP-2PLKO9E>
2017-03-18 14:20:40 +0700

    make it failed

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -2,18 +2,18 @@
 import org.junit.Assert;
 import org.junit.Test;
 
- class Person {
+class Person {
 	private double height;
 	private double weight;
-	
+
 	public Person() {
 	}
-	
+
 	public Person(double height, double weight) {
 		this.height = height;
 		this.weight = weight;
 	}
-	
+
 	public double getBMI() {
 		return this.weight / (this.height * this.height);
 	}
@@ -24,9 +24,10 @@ public class PersonTest {
 	@Test
 	public void testGetBMI() {
 		Person sut = new Person(1.8, 72.0);
-		Assert.assertEquals(22.22, sut.getBMI(), 0.1);
-		
+		Assert.assertEquals(22.22, sut.getBMI(), 0.0);
+
 		Person quang = new Person(1.62, 62.0);
 		Assert.assertEquals(23.62, quang.getBMI(), 0.01);
+		
 	}
 }
